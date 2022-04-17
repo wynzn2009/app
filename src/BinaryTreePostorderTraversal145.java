47f4dd65bc764bf66323e92c090068caa19aa0ab
wynzn2009 <wynzn29@gmail.com>
2022-04-17 14:49:05 +0800

    145

---
+++ src/BinaryTreePostorderTraversal145.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
+import pris.model.TreeNode;
+
+public class BinaryTreePostorderTraversal145 {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        help(root, list);
+        return list;
+    }
+    private void help(TreeNode root,List<Integer> list){
+        if(root==null){
+            return;
+        }
+        help(root.left,list);
+        help(root.right,list);
+        list.add(root.val);
+    }
+}
