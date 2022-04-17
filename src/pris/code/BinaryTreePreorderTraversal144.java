d82f15f57b2c73ad2b550e7ca49b61218299add4
wynzn2009 <wynzn29@gmail.com>
2022-04-17 14:47:09 +0800

    144

---
+++ src/pris/code/BinaryTreePreorderTraversal144.java
@@ -0,0 +1,22 @@
+package pris.code;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import pris.model.TreeNode;
+
+public class BinaryTreePreorderTraversal144 {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        help(root, list);
+        return list;
+    }
+    private void help(TreeNode root,List<Integer> list){
+        if(root==null){
+            return;
+        }
+        list.add(root.val);
+        help(root.left,list);
+        help(root.right,list);
+    }
+}
