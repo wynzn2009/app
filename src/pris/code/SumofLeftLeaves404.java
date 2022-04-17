6ad18caa4b40a6630b5f3138dcc10c4dcb62e83d
wynzn2009 <wynzn29@gmail.com>
2022-04-17 19:36:05 +0800

    404

---
+++ src/pris/code/SumofLeftLeaves404.java
@@ -0,0 +1,22 @@
+package pris.code;
+
+import pris.model.TreeNode;
+
+public class SumofLeftLeaves404 {
+    public int sumOfLeftLeaves(TreeNode root) {
+        if(root == null){
+            return 0;
+        }
+        if(root.left == null){
+            return sumOfLeftLeaves(root.right);
+        }
+        if(root.left != null){
+            if(root.left.left==null && root.left.right==null){
+                return root.left.val + sumOfLeftLeaves(root.right);
+            }else{
+                return sumOfLeftLeaves(root.left) + sumOfLeftLeaves(root.right);
+            }
+        }
+        return 0;
+    }
+}
