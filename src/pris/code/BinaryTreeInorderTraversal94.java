47f1d1b541df891c09767d43c4f82a27cfb8441b
123 <wangyuning0229@126.com>
2022-04-17 08:59:05 +0800

    初始化学习工程

---
+++ src/pris/code/BinaryTreeInorderTraversal94.java
@@ -0,0 +1,23 @@
+package pris.code;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import pris.model.TreeNode;
+
+public class BinaryTreeInorderTraversal94 {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> re = new ArrayList<>();
+        help(root,re);
+        return re;
+    }
+
+    private void help(TreeNode root , List<Integer> re){
+        if(null == root ){
+            return ;
+        }
+        help(root.left,re);
+        re.add(root.val);
+        help(root.right,re);
+    }
+}
