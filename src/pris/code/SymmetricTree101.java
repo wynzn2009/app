022283917b54bf9a1fdd70e09fe7a8f27733528f
wynzn2009 <wynzn29@gmail.com>
2022-04-17 09:36:48 +0800

    101

---
+++ src/pris/code/SymmetricTree101.java
@@ -0,0 +1,22 @@
+package pris.code;
+
+import pris.model.TreeNode;
+
+public class SymmetricTree101 {
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return help(root.left, root.right);
+    }
+
+    private boolean help(TreeNode left, TreeNode right) {
+        if (null == left && null == right) {
+            return true;
+        }
+        if (null == left || null == right) {
+            return false;
+        }
+        return left.val == right.val && help(left.left, right.right) && help(left.right, right.left);
+    }
+}
