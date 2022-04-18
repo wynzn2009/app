8a0d1f18c67a47e165e531755a8a3f1726a20a4c
wynzn2009 <wynzn29@gmail.com>
2022-04-18 20:24:51 +0800

    559

--- src/pris/code/BinaryTreePostorderTraversal145.java
+++ src/pris/code/BinaryTreePostorderTraversal145.java
@@ -1,3 +1,5 @@
+package pris.code;
+
 import java.util.ArrayList;
 import java.util.List;
 
---
+++ src/pris/code/MaximumDepthofNaryTree559.java
@@ -0,0 +1,21 @@
+package pris.code;
+
+import java.util.List;
+
+import pris.model.Node;
+
+public class MaximumDepthofNaryTree559 {
+
+    public int maxDepth(Node root) {
+        if (root == null) {
+            return 0;
+        }
+        List<Node> list = root.children;
+        int max = 0;
+        for (Node node : list) {
+            max = Math.max(max, maxDepth(node));
+        }
+        return max + 1;
+    }
+
+}
