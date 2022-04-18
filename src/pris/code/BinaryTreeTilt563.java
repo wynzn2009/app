765836372402142d3b2fe7672898b5a36e17ca35
wynzn2009 <wynzn29@gmail.com>
2022-04-18 20:41:10 +0800

    563

---
+++ src/pris/code/BinaryTreeTilt563.java
@@ -0,0 +1,24 @@
+package pris.code;
+
+import pris.model.TreeNode;
+
+public class BinaryTreeTilt563 {
+    int sum = 0;
+    public int findTilt(TreeNode root) {
+        if(root == null ){
+            return 0;
+        }
+        treeSum(root);
+        return sum;
+    }
+
+    private int treeSum(TreeNode root){
+        if(root == null){
+            return 0;
+        }
+        int left = treeSum(root.left);
+        int right = treeSum(root.right);
+        sum += Math.abs(left-right);
+        return root.val + left + right;
+    }
+}
