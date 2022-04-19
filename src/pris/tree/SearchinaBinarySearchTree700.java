f18ff1995f3360720b6b365f5f31cc57d98586e9
wynzn2009 <wynzn29@gmail.com>
2022-04-19 22:23:32 +0800

    700

---
+++ src/pris/tree/SearchinaBinarySearchTree700.java
@@ -0,0 +1,18 @@
+package pris.tree;
+
+import pris.model.TreeNode;
+
+public class SearchinaBinarySearchTree700 {
+    public TreeNode searchBST(TreeNode root, int val) {
+        if (root == null) {
+            return null;
+        }
+        if (root.val == val) {
+            return root;
+        } else if (root.val > val) {
+            return searchBST(root.left, val);
+        } else {
+            return searchBST(root.right, val);
+        }
+    }
+}
