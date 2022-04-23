ba03185bac061e6c20fa32160523d55434daba4f
wynzn2009 <wynzn29@gmail.com>
2022-04-23 21:16:16 +0800

    155

---
+++ src/pris/list/MinStack155.java
@@ -0,0 +1,38 @@
+package pris.list;
+
+import java.util.LinkedList;
+
+public class MinStack155 {
+    LinkedList<Integer> stack = new LinkedList<Integer>();
+    LinkedList<Integer> min = new LinkedList<Integer>();
+
+    public MinStack155() {
+
+    }
+    
+    public void push(int val) {
+        stack.push(val);
+        if(min.isEmpty() || val <= min.peek()){
+            min.push(val);
+        }
+    }
+    
+    public void pop() {
+        if(stack.isEmpty()){
+            return;
+        }
+        int val = stack.pop();
+        if(val == min.peek()){
+            min.pop();
+        }
+    }
+    
+    public int top() {
+        
+        return stack.peek();
+    }
+    
+    public int getMin() {
+        return min.peek();
+    }
+}
