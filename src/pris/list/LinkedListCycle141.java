6ca5abf73eb2bc685bfeef248fca7efb5ab98a53
wynzn2009 <wynzn29@gmail.com>
2022-04-23 19:24:47 +0800

    141

---
+++ src/pris/list/LinkedListCycle141.java
@@ -0,0 +1,21 @@
+package pris.list;
+
+import pris.model.ListNode;
+
+public class LinkedListCycle141 {
+    public boolean hasCycle(ListNode head) {
+        if(head==null || head.next==null ||head.next.next==null){
+            return false;
+        }
+        ListNode fast = head;
+        ListNode slow = head;
+        while(fast != null && fast.next!=null && fast.next.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast){
+                return true;
+            }
+        }
+        return false;
+    }
+}
