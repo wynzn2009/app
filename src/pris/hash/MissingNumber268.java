d7efa6a4a3f40a17998250b269798c53eb8e1bd6
wynzn2009 <wynzn29@gmail.com>
2022-05-01 20:11:06 +0800

    268

---
+++ src/pris/hash/MissingNumber268.java
@@ -0,0 +1,13 @@
+package pris.hash;
+
+public class MissingNumber268 {
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        int sum = n*(n+1)/2;
+        for(int i=0;i<nums.length;i++){
+            sum -= nums[i];
+        }
+
+        return sum;
+    }
+}
